7b6ddc00a13e94dc1b0b786da0a1aa301f270846
Ausar Mundra <ausar@marine-snow.co>
2023-08-01 08:22:48 -0400

    update 4.2: more rate/redirect handleing

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 from helper import constants, gsheet_helper, post_helper
-from instaloader import Instaloader, Profile, exceptions, RateController, ConnectionException
+from instaloader import Instaloader, Profile
 import gspread
 import random
 import time
@@ -8,23 +8,15 @@ import os
 USERNAME = constants.USERNAME
 PASSWORD = constants.PASSWORD
 
-class MyRateController(RateController):
-    def sleep(self, secs: float):
-        rand = float(constants.BASE + random.randint(0, constants.RAND))
-        return super().sleep(secs + rand)
-    def handle_429(self, query_type: str) -> None:
-        print('hit 429')
-        return super().handle_429(query_type)
-
 ### -- start clients --
 
-L = Instaloader(sanitize_paths=True, 
-                rate_controller=lambda ctx: MyRateController(ctx), 
-                fatal_status_codes=['302'],
+L = Instaloader(
+                sleep=True,
+                sanitize_paths=True, 
                 iphone_support=False,
                 download_videos=False,
                 max_connection_attempts=1,
-                request_timeout=1800,
+                request_timeout=1860,
             )
 sheets_client = gspread.service_account(filename='google_creds.json') # gspread client for google sheets interactions
 
@@ -46,31 +38,33 @@ def login_to_insta():
 def main():
     sheet = sheets_client.open_by_url(constants.SHEET_URL)  
     gsheet_helper.ready_gsheet(sheet=sheet)
+    print('sheet ready\n')
     login_to_insta()
-
     usernames = gsheet_helper.get_usernames_from_sheets(sheet=sheet)
+    random.shuffle(usernames)
+    print('scraping\n')
     for username in usernames: 
         if username == '':
             continue     
-        
         try:
             profile = Profile.from_username(L.context, username)
         except Exception as e:
             print(f'error: {e}')
             continue
-        time.sleep(float(5 + random.randint(0, 25)))
+        time.sleep(min(random.expovariate(0.6), 12))
         try:
             posts = profile.get_posts()
         except Exception as e:
             print(f'error: {e}')
             continue
-
         print(f'scraping {username}')
         rows_to_append = post_helper.scrape_posts(posts=posts)
-        
         gsheet_helper.send_data_to_sheets(rows_to_append=rows_to_append, sheet=sheet)
-        print('sheet updated\n')
-        #time.sleep(float(constants.BASE + constants.RAND))
+        print('sheet updated')
+
+        post_scrape_wait = float(constants.BASE + min(random.expovariate(0.6), constants.RAND))
+        print(f'waiting {post_scrape_wait} seconds before next scrape\n')
+        time.sleep(post_scrape_wait)
     print('scraping complete')
 
 if __name__ == "__main__":
