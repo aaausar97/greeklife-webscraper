85c7ec9eec5ac83f8632dd320ea2b0b3431b296f
Ausar Mundra <ausar@marine-snow.co>
2023-09-01 09:35:53 -0400

    edit docker CMDs

---
+++ app/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "running"
+sleep 60 # wait for vpn container to start
+echo "loging script running"
+python3 -m helpers.firefox_cookie
+sleep 15
+echo "scraper script running"
+python3 scraper.py
